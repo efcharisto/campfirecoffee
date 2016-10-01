47cd879a614ddf0edc921810df1135a9530febd5
Maksim Mikoss <pvl1720@gmail.com>
2016-10-01 11:39:23 -0700

    made pike place object

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict';
 
-Object pikePlace = {
-  location: 'Pike Place market';
-  minCust: 14;
-  maxCust: 35;
-  cupsCust: 1.2;
-  lbsCust: 0.34;
+var pikePlace = {
+  location: 'Pike Place market',
+  minCust: 14,
+  maxCust: 35,
+  cupsCust: 1.2,
+  lbsCust: 0.34,
 }
