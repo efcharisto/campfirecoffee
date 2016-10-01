4df09cd7ec222ba5b6ab778a03c01efe6bf29e33
Maksim Mikoss <pvl1720@gmail.com>
2016-10-01 15:01:49 -0700

    projectedCups now works with random number

--- app.js
+++ app.js
@@ -7,22 +7,24 @@ var pikePlace = {
   maxCust: 35,
   averageCups: 1.2, // 1 lbs is 16 cups
   averagePounds: 0.34,
-  hourCups: [],
-  hourPounds: [],
+  hourlyCups: [],
+  hourlyPounds: [],
   dailyCups: [],
   dailyPounds: [],
-  hourBarista: [], //need 2mins a customer
+  projectedCups: [],
+  hourlyBarista: [], //need 2mins a customer
   dailyBarista: [],
 
   hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm',
   '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm'],
 
-  randomNumber: function(min, max) {
+  randomNum: function(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
-  }
+  },
 
   projectedCups: function() {
-    console.log(this.averageCups * this.randomNum);
-  }
+    return (this.averageCups * this.randomNum(this.minCust, this.maxCust));
+  },
 
 }
+pikePlace.projectedCups();
