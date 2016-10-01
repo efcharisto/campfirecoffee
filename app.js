7667f632335f73319e3b683545023079ebb5e263
Maksim Mikoss <pvl1720@gmail.com>
2016-10-01 16:38:38 -0700

    added projected hourly customers to projected hourly cups

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var pikePlace = {
   maxCust: 35,
   averageCups: 1.2, // 1 lbs is 16 cups
   averagePounds: 0.34,
+  hourlyCust: [],
+//dailyCust: [],
   hourlyCups: [],
   hourlyPounds: [],
   dailyCups: [],
@@ -15,16 +17,23 @@ var pikePlace = {
   dailyBarista: [],
 
   hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm',
-  '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm'],
+    '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm'],
 
   randomNum: function(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
   },
 
-  projectedCups: function() {
-    this.hourlyCups.push(this.averageCups * this.randomNum(this.minCust, this.maxCust));
+  projectedHourlyCust: function() {
+    for (var i = 0; i < this.hours.length; i++) {
+      this.hourlyCust.push(this.randomNum(this.minCust, this.maxCust));
+    }
+  },
 
+  projectedHourlyCups: function() {
+    for (var i = 0; i < this.hours.length; i++) {
+      this.hourlyCups.push(this.averageCups * this.randomNum(this.minCust, this.maxCust));
+    }
   },
 
 }
-pikePlace.hourlyCups;
+pikePlace.hourlyCust;
