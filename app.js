02b28ad2ade3e879bec32cbcb35eb17d2703916b
Maksim Mikoss <pvl1720@gmail.com>
2016-10-01 12:39:44 -0700

    remade random func to be unoversal

--- app.js
+++ app.js
@@ -7,17 +7,19 @@ var pikePlace = {
   maxCust: 35,
   cupsCust: 1.2, // 1 lbs is 16 cups
   poundCust: 0.34,
-  hourCups: ;
-  hourPounds: ;
-  dailyCups: ;
-  dailyPounds: ;
-  workersHourly: ; //need 2mins a customer
-  workersDaily: ;
+  hourCups: [];
+  hourPounds: [];
+  dailyCups: [];
+  dailyPounds: [];
+  hourBarista: []; //need 2mins a customer
+  dailyBarista: [];
 
   hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm',
   '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm'],
 
-  function randomCust(minCust, maxCust) {
-  return Math.floor(Math.random() * (maxCust - minCust) + minCust);
+  function randomNum(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+
+  var projectedCups = (randomNum() * cupsCust);
 
 }
