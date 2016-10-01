ea1d57fc999d0113dd1d556e0a2703c59b0dddf8
Maksim Mikoss <pvl1720@gmail.com>
2016-10-01 12:00:15 -0700

    made variable reqs for object pike place

--- app.js
+++ app.js
@@ -2,8 +2,17 @@
 
 var pikePlace = {
   location: 'Pike Place market',
+  hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm',
+  '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm'],
   minCust: 14,
   maxCust: 35,
-  cupsCust: 1.2,
-  lbsCust: 0.34,
+  cupsCust: 1.2, // 1 lbs is 16 cups
+  poundCust: 0.34,
+  hourCups: ;
+  hourPounds: ;
+  dailyCups ;
+  dailyPounds: ;
+  workersHourly: ; //need 2mins a customer 
+  workersDaily: ;
+  var randomCust = math.Random() * (35 - 14) + 14;
 }
